e1ac309ae6f68a96a894244647755634de8a7094
CristianJPerezMorin <101406605+CristianJPerezMorin@users.noreply.github.com>
2023-11-30 17:02:06 +0100

    Realizado el Crear y Eliminar

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -7,15 +7,28 @@ public class ButtonManager : MonoBehaviour
     public GameObject MenuOpciones;
     public GameObject MenuCamara;
     public GameObject MenuCrear;
+    public GameObject MenuInformacion;
+    public GameObject MenuEliminar;
+    public GameObject MenuEditar;
 
     public bool camaraMode { get; set; }
-
     public bool createMode { get; set; }
+    public bool editMode { get; set; }
+    public bool deleteMode { get; set; }
+
+    public bool treeCreate { get; set; }
+    public bool tree2Create { get; set; }
+    public bool houseCreate { get; set; }
+    public bool specialCreate { get; set; }
 
     private void Start()
     {
         camaraMode = false;
         createMode = false;
+        treeCreate = false;
+        tree2Create = false;
+        houseCreate = false;
+        specialCreate = false;
     }
 
     public void CamaraMode()
@@ -49,4 +62,85 @@ public class ButtonManager : MonoBehaviour
         MenuCrear.SetActive(false);
         MenuOpciones.SetActive(true);
     }
+
+    public void InformationMode()
+    {
+        createMode = true;
+
+        MenuOpciones.SetActive(false);
+        MenuInformacion.SetActive(true);
+    }
+
+    public void ExitInformationMode()
+    {
+        createMode = false;
+
+        MenuInformacion.SetActive(false);
+        MenuOpciones.SetActive(true);
+    }
+
+    public void EditMode()
+    {
+        editMode = true;
+
+        MenuOpciones.SetActive(false);
+        MenuEditar.SetActive(true);
+    }
+
+    public void ExitEditMode()
+    {
+        editMode = false;
+
+        MenuEditar.SetActive(false);
+        MenuOpciones.SetActive(true);
+    }
+
+    public void DeleteMode()
+    {
+        deleteMode = true;
+
+        MenuOpciones.SetActive(false);
+        MenuEliminar.SetActive(true);
+    }
+
+    public void ExitDeleteMode()
+    {
+        deleteMode = false;
+
+        MenuEliminar.SetActive(false);
+        MenuOpciones.SetActive(true);
+    }
+
+    public void ArbolCrear()
+    {
+        treeCreate = true;
+        tree2Create = false;
+        houseCreate = false;
+        specialCreate = false;
+    }
+
+    public void Arbol2Crear()
+    {
+        treeCreate = false;
+        tree2Create = true;
+        houseCreate = false;
+        specialCreate = false;
+    }
+
+    public void CasaCrear()
+    {
+        treeCreate = false;
+        tree2Create = false;
+        houseCreate = true;
+        specialCreate = false;
+    }
+
+    public void EspecialCrear()
+    {
+        treeCreate = false;
+        tree2Create = false;
+        houseCreate = false;
+        specialCreate = true;
+    }
 }
+
--- Assets/Scripts/CamaraAcciones.cs
+++ Assets/Scripts/CamaraAcciones.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class CamaraAcciones : MonoBehaviour
 {
+    public GameObject prefabTree;
+    public GameObject prefabTree2;
+    public GameObject prefabHouse;
+    public GameObject prefabSpecial;
+
+    public TMP_Text arbolText;
+    public TMP_Text arbol2Text;
+    public TMP_Text houseText;
+    public TMP_Text specialText;
+
     ButtonManager _buttonManager;
     public GameObject controller;
 
     float rotacionX = 0f;
     float rotacionY = 0f;
     float zoom = 110;
+
+    int layerMask = 1 << 6;
 
     void Start()
     {
@@ -18,6 +32,11 @@ public class CamaraAcciones : MonoBehaviour
 
     void Update()
     {
+        arbolText.text = "Hay un total de " + GameObject.FindGameObjectsWithTag("Arbol").Length + " árbol";
+        arbol2Text.text = "Hay un total de " + GameObject.FindGameObjectsWithTag("Arbol2").Length + " árbol2";
+        houseText.text = "Hay un total de " + GameObject.FindGameObjectsWithTag("Casa").Length + " casas";
+        specialText.text = "Hay un total de " + GameObject.FindGameObjectsWithTag("Especial").Length + " espadas";
+
         if (_buttonManager.camaraMode)
         {
             float mouseX = Input.GetAxis("Mouse X");
@@ -43,9 +62,63 @@ public class CamaraAcciones : MonoBehaviour
 
         if (_buttonManager.createMode)
         {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.SphereCast(rayo, 20, out hit, Mathf.Infinity))
+                {
+                    if(hit.collider.gameObject.tag == "Terreno")
+                    {
+                        CreateObject(hit.point, new Quaternion(0, 0, 0, 0));
+                    }
+                }
+            }
+        }
+
+        if (_buttonManager.editMode)
+        {
 
         }
 
+        if (_buttonManager.deleteMode)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(rayo, out hit, Mathf.Infinity, layerMask))
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+
+    }
+
+    private void CreateObject(Vector3 position, Quaternion rotation)
+    {
+        if (_buttonManager.treeCreate)
+        {
+            Instantiate(prefabTree, position, rotation);
+        }
+
+        if (_buttonManager.tree2Create)
+        {
+            Instantiate(prefabTree2, position, rotation);
+        }
+
+        if (_buttonManager.houseCreate)
+        {
+            Instantiate(prefabHouse, position, rotation);
+        }
+
+        if (_buttonManager.specialCreate)
+        {
+            Instantiate(prefabSpecial, position, rotation);
+        }
     }
 
     private void OnDrawGizmos()
